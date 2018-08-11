e1c55ded0d6f7ce4a95f08958e3d06317b038155
hmesander <haleymesander@gmail.com>
2018-08-11 09:34:50 -0600

    Refactor to change for loop to while loop

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,8 +1,12 @@
 function bubbleSort(array) {
-  for (let n=0; n < (array.length - 1); n++) {
-    for (let i=0; i < (array.length - n); i++) {
+  unsorted = true
+
+  while (unsorted) {
+    unsorted = false;
+    for (let i=0; i < (array.length - 1); i++) {
       if (array[i] > array[i+1]) {
         [ array[i], array[i+1] ] = [ array[i+1], array[i] ];
+        unsorted = true
       }
     }
   }
